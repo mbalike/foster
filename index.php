5ca4baad5a45765367871c67e7af980dc47637c4
mbalike <dmbalike@outlook.com>
2024-09-06 13:01:14 +0300

    Upadated

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Welcome!</title>
 </head>
 <body>
     <h1>Hello, Login below</h1>
+    <h4>Add it to git</h4>
+    <h4>I likes it</h4>
     <a href="login.php">Here</a>
 </body>
 </html>
